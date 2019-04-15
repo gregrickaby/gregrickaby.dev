2622d207c851a2045437d6a40c75dd75b7a9d329
Greg Rickaby <gregrickaby@gmail.com>
2019-04-15 14:41:57 -0500

    add ContactForm

---
+++ src/components/ContactForm/index.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { ContactForm } from "./styles"
+
+const ContactForm = props => (
+  <ContactForm>
+    <form
+      name="contact"
+      method="post"
+      data-netlify="true"
+      data-netlify-honeypot="bot-field"
+    >
+      <input type="hidden" name="bot-field" />
+      <div className="field first">
+        <label htmlFor="name">Name</label>
+        <input type="text" name="name" id="name" />
+      </div>
+      <div className="field">
+        <label htmlFor="email">Email</label>
+        <input type="text" name="email" id="email" />
+      </div>
+      <div className="field">
+        <label htmlFor="message">Message</label>
+        <textarea name="message" id="message" rows="6" />
+      </div>
+      <ul className="actions">
+        <li>
+          <input type="submit" value="Send Message" />
+        </li>
+        <li>
+          <input type="reset" value="Clear" />
+        </li>
+      </ul>
+    </form>
+  </ContactForm>
+)
+
+export default ContactForm
