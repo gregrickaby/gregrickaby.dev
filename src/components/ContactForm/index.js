338e5d803c32ca6fcd38f78df776013c1a56b01e
Greg Rickaby <gregrickaby@gmail.com>
2019-04-15 22:07:11 -0500

    add basic form validation

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -10,16 +10,32 @@ const ContactForm = props => (
       data-netlify-honeypot="bot-field"
     >
       <div className="field name">
-        <label htmlFor="name">Name</label>
-        <input type="text" name="name" id="name" />
+        <label htmlFor="name">
+          Name <span>*</span>
+        </label>
+        <input type="text" name="name" id="name" required minlength="2" />
       </div>
       <div className="field email">
-        <label htmlFor="email">Email</label>
-        <input type="text" name="email" id="email" />
+        <label htmlFor="email">
+          Email <span>*</span>
+        </label>
+        <input
+          type="text"
+          name="email"
+          id="email"
+          required
+          pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$"
+        />
+      </div>
+      <div className="field file">
+        <label htmlFor="file">File</label>
+        <input type="file" name="file" id="file" />
       </div>
       <div className="field message">
-        <label htmlFor="message">Message</label>
-        <textarea name="message" id="message" rows="6" />
+        <label htmlFor="message">
+          Message <span>*</span>
+        </label>
+        <textarea name="message" id="message" rows="6" required minlength="6" />
       </div>
       <div className="field submit">
         <input type="submit" value="Send Message" />
