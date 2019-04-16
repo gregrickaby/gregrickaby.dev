bbc05b7f08e5a0ef30a22df9fc83b20870c66e03
Greg Rickaby <gregrickaby@gmail.com>
2019-04-16 18:14:49 -0500

    basic menu styles

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,10 +1,19 @@
+import { Link } from "gatsby"
 import React from "react"
 import { MenuLayout } from "./styles"
 
 const Menu = props => (
   <MenuLayout>
     <ul>
-      <li>Hi</li>
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/about">About</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contact</Link>
+      </li>
     </ul>
   </MenuLayout>
 )
--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,7 +1,15 @@
 import styled from "styled-components"
 
 export const MenuLayout = styled.nav`
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
   li {
     color: #fff;
+    display: inline-flex;
+    margin-right: 12px;
   }
 `
