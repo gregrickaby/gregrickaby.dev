fc35d68a2d5d9976a0a9de818d44341e790adf71
Greg Rickaby <gregrickaby@gmail.com>
2019-04-16 19:11:56 -0500

    write a bio

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,23 +1,70 @@
+import { Link } from "gatsby"
+import Img from "gatsby-image"
 import React from "react"
-
+import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import SEO from "../components/Seo"
 
-const About = ({ location }) => (
+const About = ({ location, data }) => (
   <Layout location={location}>
-    <SEO title="About Us" />
+    <SEO title="About Greg" />
     <h1>About</h1>
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliter enim
-      nosmet ipsos nosse non possumus. Quis enim redargueret? Nonne videmus
-      quanta perturbatio rerum omnium consequatur, quanta confusio? Itaque eos
-      id agere, ut a se dolores, morbos, debilitates repellant. Vitae autem
-      degendae ratio maxime quidem illis placuit quieta. Quae cum magnifice
-      primo dici viderentur, considerata minus probabantur. Atque haec
-      coniunctio confusioque virtutum tamen a philosophis ratione quadam
-      distinguitur.{" "}
+      I’m the Director of Engineering at WebDevStudios, where we build amazing
+      digital platforms for large and enterprise level businesses.
+    </p>
+    <Img
+      className="align-right"
+      alt="The Rickaby Family"
+      fixed={data.familyImage.childImageSharp.fixed}
+    />
+    <p>
+      While at WebDevStudios, my engineers and I work with clients like
+      Microsoft Office, Viacom, NBA, MTV News, Paramount Network, Uber, Skype,
+      Skype in Media, Campbell’s Soup, Pepperidge Farm, H.E.B. Central Market,
+      Realtor.com, Kaspersky, and The Discovery Channel.
+    </p>
+    <p>
+      Over the years, I’ve also contributed to WordPress Core, various WordPress
+      themes, dozens of open-source projects, including GatsbyJS, and have
+      spoken at a few WordCamps.
+    </p>
+    <p>
+      I’ve also written a children’s book about building your first website,
+      which is on sale at Barnes & Noble and online at Amazon.com.
+    </p>
+    <p>
+      I’m also always trying to learn something new, everything from leadership
+      skills, or mastering new technology, to playing music. Right now, I’m “all
+      in” on GraphQL, React, and GatsbyJS. You can learn more about my
+      professional skill-set at LinkedIn, Github, and WordPress.org.
+    </p>
+    <p>
+      When I’m not at work? I enjoy being a dad and hanging out with family, as
+      well as serving my community through Wiregrass Church. I’ve also been
+      known to make delicious pizzas.
+    </p>
+    <p>
+      If you’re looking to hire or retain an experienced web developer?{" "}
+      <Link to="/contact"> am available.</Link>
+    </p>
+    <p>
+      My services include hosting, server management, plugin development, PSD
+      conversions, and theme creation. There really isn’t much I can’t do!
     </p>
   </Layout>
 )
 
 export default About
+
+export const query = graphql`
+  query {
+    familyImage: file(relativePath: { eq: "rickaby-family-2018.jpg" }) {
+      childImageSharp {
+        fixed(width: 360) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
