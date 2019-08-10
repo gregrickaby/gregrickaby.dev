7d8df8b8c0e350a7b93316ddf4c07525cf7c1324
Greg Rickaby <gregrickaby@gmail.com>
2019-08-09 20:06:48 -0500

    bring in theme-ui

--- gatsby-config.js
+++ gatsby-config.js
@@ -9,6 +9,7 @@ module.exports = {
     `gatsby-plugin-sitemap`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-emotion`,
+    `gatsby-plugin-theme-ui`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
