2d1a4268203cbbac9e1c303193797dd1197eea69
Greg Rickaby <gregrickaby@gmail.com>
2019-08-09 20:49:27 -0500

    add dark mode toggler

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,26 +3,37 @@ import PropTypes from "prop-types"
 import React from "react"
 import Menu from "../Menu/"
 import { HeaderWrapper, HeaderContainer } from "./styles"
+import { useColorMode } from "theme-ui"
 
-const Header = ({ siteTitle, siteDescription }) => (
-  <HeaderWrapper className="site-header">
-    <HeaderContainer>
-      <h1>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
+const Header = ({ siteTitle, siteDescription }) => {
+  const [colorMode, setColorMode] = useColorMode()
+  return (
+    <HeaderWrapper className="site-header">
+      <HeaderContainer>
+        <button
+          onClick={e => {
+            setColorMode(colorMode === "light" ? "dark" : "light")
           }}
         >
-          {siteTitle}
-        </Link>
-      </h1>
-      <p>{siteDescription}</p>
-      <Menu />
-    </HeaderContainer>
-  </HeaderWrapper>
-)
+          Toggle {colorMode === "light" ? "Dark" : "Light"}
+        </button>
+        <h1>
+          <Link
+            to="/"
+            style={{
+              color: `white`,
+              textDecoration: `none`,
+            }}
+          >
+            {siteTitle}
+          </Link>
+        </h1>
+        <p>{siteDescription}</p>
+        <Menu />
+      </HeaderContainer>
+    </HeaderWrapper>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
